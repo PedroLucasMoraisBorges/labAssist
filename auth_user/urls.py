f97d6e1471e796c814bab59cff93f5dfc22cdd7d
Pedro Lucas <pedrolucasmoraisborges@gmail.com>
2024-08-28 21:10:36 -0300

    fear(authentication)

--- auth_user/urls.py
+++ auth_user/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('login/', Login.as_view(), name='login'),
+    path('cadastro/', RegisterUser.as_view(), name='register')
 ]
