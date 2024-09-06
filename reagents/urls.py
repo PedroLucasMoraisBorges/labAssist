485feea8bcccf67106eaf1eab45c968208673d57
Pedro Lucas <pedrolucasmoraisborges@gmail.com>
2024-09-06 15:38:55 -0300

    feat(home_admin)

--- reagents/urls.py
+++ reagents/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('landingPage', LandingPage.as_view(), name='landing_page'),
+    path('home/', HomeAdmin.as_view(), name='home_admin'),
     path('cadastrarReagente/', RegisterReagent.as_view(), name='register_reagent'),
 ]
