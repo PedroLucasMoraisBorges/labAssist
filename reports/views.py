8b78001c30770c77723c67a291d9ae8c8c7b0184
Pedro Lucas <pedrolucasmoraisborges@gmail.com>
2024-10-18 17:10:50 -0300

    feat(standardization)

--- reports/views.py
+++ reports/views.py
@@ -19,7 +19,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.utils.decorators import method_decorator
 from auth_user.decorators import *
 
-class Requests(View):
+class Pendings(View):
     @method_decorator(login_required)
     @method_decorator(superuser_required)
     def get(self, request):
